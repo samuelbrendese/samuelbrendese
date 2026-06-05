733320d11b63c9fd977bc4a97424bfdf82aacc12
Samuel Brendese <sambrendese@gmail.com>
2026-06-05 14:41:32 -0400

    Always scroll to top on page navigation

--- src/components/ScrollRestoration.js
+++ src/components/ScrollRestoration.js
@@ -5,30 +5,8 @@ export function ScrollRestoration() {
   const location = useLocation();
 
   useEffect(() => {
-    // Restore scroll position when route changes
-    const savedScrollPosition = sessionStorage.getItem(`scrollPos_${location.pathname}`);
-    if (savedScrollPosition) {
-      setTimeout(() => {
-        window.scrollTo(0, parseInt(savedScrollPosition, 10));
-      }, 0);
-    } else {
-      window.scrollTo(0, 0);
-    }
-  }, [location.pathname]);
-
-  useEffect(() => {
-    // Save scroll position before page reload or navigation
-    const handleSaveScroll = () => {
-      sessionStorage.setItem(`scrollPos_${location.pathname}`, window.scrollY.toString());
-    };
-
-    window.addEventListener('beforeunload', handleSaveScroll);
-    
-    // Also save on route change
-    return () => {
-      handleSaveScroll();
-      window.removeEventListener('beforeunload', handleSaveScroll);
-    };
+    // Always scroll to top when route changes
+    window.scrollTo(0, 0);
   }, [location.pathname]);
 
   return null;
